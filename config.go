260127bf8da046b9479853331355fc154e6edc9c
Jon Bonazza <jonbonazza@gmail.com>
2016-03-23 01:31:32 -0700

    fixed typo in config field

--- config.go
+++ config.go
@@ -2,5 +2,5 @@ package kaiju
 
 type Config struct {
 	BindHost string `yaml:"bindHost" json:"bindHost" envconfig:"bind_host" default:"localhost"`
-	Port     int    `yaml:"bindPort" json:"bindPort" enconfig:"bind_port" default:"8080"`
+	Port     int    `yaml:"bindPort" json:"bindPort" envconfig:"bind_port" default:"8080"`
 }
